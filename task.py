1e28abb0d92d3aad28cfa282fc7bf67cf912fada
Jan Kot <janymalino@gmail.com>
2023-10-24 13:25:00 +0200

    initial

--- task.py
+++ task.py
@@ -1,26 +1,43 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+#!/usr/bin/env python3
+
+from uuid import uuid4 as generate_uuid
+
+
+class Account:
+    def __init__(self, first_name: str, last_name: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.balance = 0
+
+    def input(self, amount: float):
+        self.balance += amount
+
+    def withdraw(self, amount: float):
+        if amount > self.balance:
+            raise ValueError("the amount greater than the balance")
+        self.balance -= amount
+
+
+class Bank:
+    def __init__(self, name: str):
+        self.name = name
+        self.accounts = {}
+
+    def create_account(self, first_name: str, last_name: str):
+        new_account = Account(first_name, last_name)
+        new_account_address = generate_uuid().hex
+        while new_account_address in self.accounts.keys:
+            new_account_address = generate_uuid().hex
+        self.accounts[new_account_address] = new_account
+
+    def transfer(self, form_address: str, to_address: str, amount: float):
+        if from_address not in self.accounts.keys:
+            raise ValueError("from address not found")
+        if to_address not in self.accounts.keys:
+            raise ValueError("to address not found")
+        self.accounts[from_address].withdraw(amount)
+        self.accounts[to_address].input(amount)
+
+
+if __name__ == "__main__":
+    pass
